4386c3c8bd739dc649fc006193a67bcc66c80a76
WesleyBanagouro <wesley.banagouro@outlook.com>
2023-10-27 14:11:55 -0300

    interatividade feita

--- interatividade.js
+++ interatividade.js
@@ -1,12 +1,22 @@
-const paineis = document.querySelectorAll('.panel');
+let aberto = null;
 
 function clicar() {
-    console.log('Deu cert');
+    if (aberto === this) {
+        console.log('São iguais');
+    } else {
+        console.log('São diferentes');
+        if(aberto !== null) {
+            aberto.classList.toggle('open');
+            aberto.classList.toggle('open-active');
+        }
+        aberto = this;
+    }
+    this.classList.toggle('open');
+    this.classList.toggle('open-active');
 };
 
+const paineis = document.querySelectorAll('.panel');
 for(var i = 0; i < paineis.length; i++) {
-    const painel = paineis[i]
+    const painel = paineis[i];
     painel.addEventListener("click", clicar);
 }
-
-
